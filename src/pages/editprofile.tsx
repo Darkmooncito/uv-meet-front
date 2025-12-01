30f2b64ee552afddfcfc6644d6e3f6c0b2a765ca
joshuaarciniegas <joshua.arciniegas@correounivalle.edu.co>
2025-11-30 21:49:24 -0500

    build

--- src/pages/editprofile.tsx
+++ src/pages/editprofile.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+
 import "../styles/editprofile.sass";
 
 const EditarPerfil = () => {
--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+
 import "../styles/profile.sass";
 
 const Perfil = () => {
